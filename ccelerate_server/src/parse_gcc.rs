f2f0e0228753e8ffd2ffd08c5b20d350a5b0acc9
Jacques Lucke <mail@jlucke.com>
2025-03-02 14:10:53 +0100

    test for linking shared object

--- ccelerate_server/src/parse_gcc.rs
+++ ccelerate_server/src/parse_gcc.rs
@@ -23,6 +23,7 @@ pub struct GCCArgs {
     pub depfile_output_path: Option<PathBuf>,
     pub depfile_target_name: Option<String>,
     pub compile_only: bool,
+    pub shared: bool,
 }
 
 impl Default for GCCArgs {
@@ -43,6 +44,7 @@ impl Default for GCCArgs {
             depfile_output_path: None,
             depfile_target_name: None,
             compile_only: false,
+            shared: false,
         }
     }
 }
@@ -76,6 +78,8 @@ impl GCCArgs {
                 args.machine_args.push(arg_str.to_string());
             } else if arg_str == "-pipe" {
                 args.pipe = true;
+            } else if arg_str == "-shared" {
+                args.shared = true;
             } else if arg_str.starts_with("-f") {
                 args.f_flags.push(arg_str.to_string());
             } else if arg_str.starts_with("-O") {
@@ -116,6 +120,9 @@ impl GCCArgs {
         }
         if self.pipe {
             args.push("-pipe".into());
+        }
+        if self.shared {
+            args.push("-shared".into());
         }
         if let Some(lang_std) = &self.lang_std {
             args.push(lang_std.into());
@@ -169,7 +176,7 @@ impl GCCArgs {
 }
 
 #[test]
-fn test_gcc_compile_translation_unit_task() {
+fn test_parse_gcc_compile_args_for_compilation() {
     let raw_args = vec![
         "-DHAVE_EXECINFO_H",
         "-DHAVE_MALLOC_STATS_H",
@@ -278,17 +285,18 @@ fn test_gcc_compile_translation_unit_task() {
     assert_eq!(
         args.user_includes,
         vec![
-            PathBuf::from("/home/jacques/blender/blender/source/blender/makesdna"),
-            PathBuf::from(
-                "/home/jacques/Documents/ccelerate_test/build_blender/source/blender/makesdna/intern"
-            ),
-            PathBuf::from("/home/jacques/blender/blender/source/blender/blenlib"),
-            PathBuf::from("/home/jacques/blender/blender/source/blender/imbuf"),
-            PathBuf::from("/home/jacques/blender/blender/source/blender/imbuf/movie"),
-            PathBuf::from("/home/jacques/blender/blender/intern/atomic/."),
-            PathBuf::from("/home/jacques/blender/blender/intern/guardedalloc"),
-            PathBuf::from("/home/jacques/blender/blender/extern/fmtlib/include"),
+            "/home/jacques/blender/blender/source/blender/makesdna",
+            "/home/jacques/Documents/ccelerate_test/build_blender/source/blender/makesdna/intern",
+            "/home/jacques/blender/blender/source/blender/blenlib",
+            "/home/jacques/blender/blender/source/blender/imbuf",
+            "/home/jacques/blender/blender/source/blender/imbuf/movie",
+            "/home/jacques/blender/blender/intern/atomic/.",
+            "/home/jacques/blender/blender/intern/guardedalloc",
+            "/home/jacques/blender/blender/extern/fmtlib/include",
         ]
+        .iter()
+        .map(|s| PathBuf::from(s))
+        .collect::<Vec<_>>()
     );
     assert_eq!(
         args.system_includes,
@@ -299,24 +307,124 @@ fn test_gcc_compile_translation_unit_task() {
     assert_eq!(
         args.defines,
         vec![
-            "HAVE_EXECINFO_H".to_string(),
-            "HAVE_MALLOC_STATS_H".to_string(),
-            "NDEBUG".to_string(),
-            "WITH_ASSERT_ABORT".to_string(),
-            "WITH_DNA_GHASH".to_string(),
-            "WITH_FREESTYLE".to_string(),
-            "WITH_GHOST_WAYLAND_LIBDECOR".to_string(),
-            "WITH_TBB".to_string(),
-            "_FILE_OFFSET_BITS=64".to_string(),
-            "_LARGEFILE64_SOURCE".to_string(),
-            "_LARGEFILE_SOURCE".to_string(),
-            "__LITTLE_ENDIAN__".to_string(),
-            "NDEBUG".to_string(),
+            "HAVE_EXECINFO_H",
+            "HAVE_MALLOC_STATS_H",
+            "NDEBUG",
+            "WITH_ASSERT_ABORT",
+            "WITH_DNA_GHASH",
+            "WITH_FREESTYLE",
+            "WITH_GHOST_WAYLAND_LIBDECOR",
+            "WITH_TBB",
+            "_FILE_OFFSET_BITS=64",
+            "_LARGEFILE64_SOURCE",
+            "_LARGEFILE_SOURCE",
+            "__LITTLE_ENDIAN__",
+            "NDEBUG",
         ]
+        .iter()
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>()
     );
     assert_eq!(args.opt_flags, vec!["-O2".to_string()]);
     assert_eq!(args.machine_args, vec!["-march=x86-64-v2".to_string()]);
 
+    test_round_trip(&args);
+}
+
+#[test]
+fn test_parse_gcc_compile_args_for_so_linking() {
+    let raw_args = vec![
+        "-fPIC",
+        "-Wuninitialized",
+        "-Wall",
+        "-Wno-invalid-offsetof",
+        "-Wno-sign-compare",
+        "-Wlogical-op",
+        "-Winit-self",
+        "-Wmissing-include-dirs",
+        "-Wno-div-by-zero",
+        "-Wtype-limits",
+        "-Wno-char-subscripts",
+        "-Wno-unknown-pragmas",
+        "-Wpointer-arith",
+        "-Wcomma-subscript",
+        "-Wformat-signedness",
+        "-Wrestrict",
+        "-Wno-suggest-override",
+        "-Wuninitialized",
+        "-Wno-stringop-overread",
+        "-Wno-stringop-overflow",
+        "-Wimplicit-fallthrough=5",
+        "-march=x86-64-v2",
+        "-pipe",
+        "-fPIC",
+        "-funsigned-char",
+        "-fno-strict-aliasing",
+        "-ffp-contract=off",
+        "-fmacro-prefix-map=/home/jacques/blender/blender/=",
+        "-fmacro-prefix-map=/home/jacques/Documents/ccelerate_test/build_blender/=",
+        "-Wno-deprecated-declarations",
+        "-Wno-unused-parameter",
+        "-Wno-unused-function",
+        "-Wno-type-limits",
+        "-Wno-int-in-bool-context",
+        "-Wno-format",
+        "-Wno-switch",
+        "-Wno-unused-variable",
+        "-Wno-uninitialized",
+        "-Wno-implicit-fallthrough",
+        "-Wno-error=unused-but-set-variable",
+        "-Wno-class-memaccess",
+        "-Wno-comment",
+        "-Wno-unused-local-typedefs",
+        "-Wno-unused-variable",
+        "-Wno-uninitialized",
+        "-Wno-maybe-uninitialized",
+        "-O2",
+        "-DNDEBUG",
+        "-shared",
+        "-Wl,-soname,libextern_draco.so",
+        "-o",
+        "lib/libextern_draco.so",
+        "extern/draco/CMakeFiles/extern_draco.dir/src/common.cpp.o",
+        "extern/draco/CMakeFiles/extern_draco.dir/src/decoder.cpp.o",
+        "extern/draco/CMakeFiles/extern_draco.dir/src/encoder.cpp.o",
+        "-Wl,-rpath,$ORIGIN/lib:/home/jacques/Documents/ccelerate_test/build_blender/bin/lib",
+        "lib/libdraco.a",
+    ];
+    let raw_args: Vec<&OsStr> = raw_args.iter().map(|s| s.as_ref()).collect();
+
+    let args = GCCArgs::parse(
+        &Path::new("/home/jacques/Documents/ccelerate_test/build_blender"),
+        &raw_args,
+    );
+    assert!(args.is_ok());
+    let args = args.unwrap();
+
+    assert!(args.shared);
+    assert_eq!(
+        args.sources,
+        vec![
+            "/home/jacques/Documents/ccelerate_test/build_blender/extern/draco/CMakeFiles/extern_draco.dir/src/common.cpp.o",
+            "/home/jacques/Documents/ccelerate_test/build_blender/extern/draco/CMakeFiles/extern_draco.dir/src/decoder.cpp.o",
+            "/home/jacques/Documents/ccelerate_test/build_blender/extern/draco/CMakeFiles/extern_draco.dir/src/encoder.cpp.o",
+            "/home/jacques/Documents/ccelerate_test/build_blender/lib/libdraco.a",
+        ].iter().map(|s| Path::new(s)).collect::<Vec<_>>()
+    );
+    assert_eq!(args.compile_only, false);
+    assert_eq!(args.depfile_generate, false);
+    assert_eq!(
+        args.primary_output,
+        Some(PathBuf::from(
+            "/home/jacques/Documents/ccelerate_test/build_blender/lib/libextern_draco.so"
+        ))
+    );
+    assert_eq!(args.defines, vec!["NDEBUG".to_string()]);
+
+    test_round_trip(&args);
+}
+
+fn test_round_trip(args: &GCCArgs) {
     let to_args_result = args.to_args();
     let parsed_again = GCCArgs::parse(
         &Path::new("/some/other/path"),
@@ -328,5 +436,5 @@ fn test_gcc_compile_translation_unit_task() {
     );
     assert!(parsed_again.is_ok());
     let parsed_again = parsed_again.unwrap();
-    assert_eq!(parsed_again, args);
+    assert_eq!(&parsed_again, args);
 }
