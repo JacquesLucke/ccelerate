6555f8029f263f2ae19a2d6b238ac88ba1d5a205
Jacques Lucke <mail@jlucke.com>
2025-03-02 01:26:42 +0100

    initial argument parsing

--- ccelerate_server/src/main.rs
+++ ccelerate_server/src/main.rs
@@ -1,7 +1,147 @@
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
+
 use actix_web::HttpResponse;
 use anyhow::Result;
 use base64::prelude::*;
 use ccelerate_shared::RunRequestData;
+use parking_lot::Mutex;
+use rusqlite_migration::{M, Migrations};
+
+struct State {
+    conn: Arc<Mutex<rusqlite::Connection>>,
+}
+
+#[derive(Debug)]
+struct FileToObjectFileCommand {
+    cwd: PathBuf,
+    binary: String,
+    args: Vec<String>,
+    input: PathBuf,
+    output: PathBuf,
+}
+
+#[derive(Debug)]
+struct ArchiveCommand {
+    cwd: PathBuf,
+    binary: String,
+    args: Vec<String>,
+    input_libs: Vec<PathBuf>,
+    input_objects: Vec<PathBuf>,
+    output: PathBuf,
+}
+
+#[derive(Debug)]
+struct BinaryCommand {
+    cwd: PathBuf,
+    binary: String,
+    args: Vec<String>,
+    inputs: Vec<PathBuf>,
+    output: PathBuf,
+}
+
+#[derive(Debug)]
+struct UnknownCommand {
+    _cwd: PathBuf,
+    _binary: String,
+    _args: Vec<String>,
+}
+
+#[derive(Debug)]
+enum Command {
+    FileToOjectFile(FileToObjectFileCommand),
+    Archive(ArchiveCommand),
+    Binary(BinaryCommand),
+    Unknown(UnknownCommand),
+}
+
+fn make_absolute(base: &Path, path: &Path) -> PathBuf {
+    if path.is_absolute() {
+        return path.to_path_buf();
+    }
+    base.join(path)
+}
+
+fn parse_command(run_request: &RunRequestData) -> Command {
+    if ["gcc", "g++", "clang", "clang++"].contains(&run_request.binary.as_str()) {
+        if run_request.args.iter().any(|arg| arg == "-c") {
+            if let Some(out_pos) = run_request.args.iter().position(|arg| arg == "-o") {
+                if let Some(out_arg) = &run_request.args.get(out_pos + 1) {
+                    let output = make_absolute(&run_request.cwd, Path::new(out_arg));
+                    let input = make_absolute(
+                        &run_request.cwd,
+                        Path::new(run_request.args.last().unwrap()),
+                    );
+                    return Command::FileToOjectFile(FileToObjectFileCommand {
+                        cwd: run_request.cwd.clone(),
+                        binary: run_request.binary.clone(),
+                        args: run_request.args.clone(),
+                        input: input.clone(),
+                        output,
+                    });
+                }
+            }
+        } else {
+            if let Some(out_pos) = run_request.args.iter().position(|arg| arg == "-o") {
+                if let Some(out_arg) = &run_request.args.get(out_pos + 1) {
+                    let output = make_absolute(&run_request.cwd, Path::new(out_arg));
+                    let inputs = run_request
+                        .args
+                        .iter()
+                        .filter(|a| {
+                            a.ends_with(".o")
+                                || a.ends_with(".a")
+                                || a.ends_with(".so")
+                                || a.ends_with(".c")
+                                || a.ends_with(".cc")
+                                || a.ends_with(".cpp")
+                                || a.ends_with(".cxx")
+                        })
+                        .map(|a| make_absolute(&run_request.cwd, Path::new(a)))
+                        .collect();
+                    return Command::Binary(BinaryCommand {
+                        cwd: run_request.cwd.clone(),
+                        binary: run_request.binary.clone(),
+                        args: run_request.args.clone(),
+                        inputs: inputs,
+                        output,
+                    });
+                }
+            }
+        }
+    }
+    if run_request.binary == "ar" {
+        let libs: Vec<PathBuf> = run_request
+            .args
+            .iter()
+            .filter(|a| a.ends_with(".a"))
+            .map(|a| PathBuf::from(a))
+            .collect();
+        let output_lib = libs[0].clone();
+        let inputs_libs: Vec<PathBuf> = libs[1..].to_vec();
+        let inputs_objects: Vec<PathBuf> = run_request
+            .args
+            .iter()
+            .filter(|a| a.ends_with(".o"))
+            .map(|a| PathBuf::from(a))
+            .collect();
+        return Command::Archive(ArchiveCommand {
+            cwd: run_request.cwd.clone(),
+            binary: run_request.binary.clone(),
+            args: run_request.args.clone(),
+            input_libs: inputs_libs,
+            input_objects: inputs_objects,
+            output: output_lib,
+        });
+    }
+    Command::Unknown(UnknownCommand {
+        _cwd: run_request.cwd.clone(),
+        _binary: run_request.binary.clone(),
+        _args: run_request.args.clone(),
+    })
+}
 
 #[actix_web::get("/")]
 async fn route_index() -> impl actix_web::Responder {
@@ -22,9 +162,62 @@ fn need_eager_evaluation(run_request: &RunRequestData) -> bool {
 }
 
 #[actix_web::post("/run")]
-async fn route_run(run_request: actix_web::web::Json<RunRequestData>) -> impl actix_web::Responder {
-    println!("{:?}", run_request);
+async fn route_run(
+    run_request: actix_web::web::Json<RunRequestData>,
+    state: actix_web::web::Data<State>,
+) -> impl actix_web::Responder {
     let eager_evaluation = need_eager_evaluation(&run_request);
+
+    let parsed_command = parse_command(&run_request);
+    match parsed_command {
+        Command::FileToOjectFile(command) => {
+            let conn = state.conn.lock();
+            conn.execute(
+                "INSERT OR REPLACE INTO ObjectFiles (output_path, input_path, cwd, binary, args) VALUES (?1, ?2, ?3, ?4, ?5)",
+                rusqlite::params![
+                    command.output.to_str().unwrap(),
+                    command.input.to_str().unwrap(),
+                    command.cwd.to_str().unwrap(),
+                    command.binary,
+                    serde_json::to_string(&command.args).unwrap()
+                ],
+            )
+            .unwrap();
+        }
+        Command::Archive(command) => {
+            let conn = state.conn.lock();
+            conn.execute(
+                "INSERT OR REPLACE INTO ArchiveFiles (output_path, binary, args, cwd, input_libs, input_objects) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+                rusqlite::params![
+                    command.output.to_str().unwrap(),
+                    command.binary,
+                    serde_json::to_string(&command.args).unwrap(),
+                    command.cwd.to_str().unwrap(),
+                    serde_json::to_string(&command.input_libs).unwrap(),
+                    serde_json::to_string(&command.input_objects).unwrap(),
+                ],
+            )
+            .unwrap();
+        }
+        Command::Binary(command) => {
+            let conn = state.conn.lock();
+            conn.execute(
+                "INSERT OR REPLACE INTO BinaryFiles (output_path, binary, args, cwd, inputs) VALUES (?1, ?2, ?3, ?4, ?5)",
+                rusqlite::params![
+                    command.output.to_str().unwrap(),
+                    command.binary,
+                    serde_json::to_string(&command.args).unwrap(),
+                    command.cwd.to_str().unwrap(),
+                    serde_json::to_string(&command.inputs).unwrap(),
+                ],
+            )
+            .unwrap();
+        }
+        Command::Unknown(unknown) => {
+            println!("{:?}", unknown);
+        }
+    }
+
     let Ok(command) = tokio::process::Command::new(&run_request.binary)
         .args(&run_request.args)
         .current_dir(&run_request.cwd)
@@ -62,8 +255,49 @@ async fn route_run(run_request: actix_web::web::Json<RunRequestData>) -> impl ac
 async fn main() -> Result<()> {
     let addr = "127.0.0.1:6235";
     println!("Listening on http://{}", addr);
-    actix_web::HttpServer::new(|| {
+
+    let db_migrations = Migrations::new(vec![
+        M::up(
+            "CREATE TABLE ObjectFiles(
+                output_path TEXT NOT NULL PRIMARY KEY,
+                input_path TEXT NOT NULL,
+                cwd TEXT NOT NULL,
+                binary TEXT NOT NULL,
+                args JSON NOT NULL
+            );",
+        ),
+        M::up(
+            "CREATE TABLE ArchiveFiles(
+                output_path TEXT NOT NULL PRIMARY KEY,
+                binary TEXT NOT NULL,
+                args JSON NOT NULL,
+                cwd TEXT NOT NULL,
+                input_libs JSON NOT NULL,
+                input_objects JSON NOT NULL
+            );",
+        ),
+        M::up(
+            "CREATE TABLE BinaryFiles(
+                output_path TEXT NOT NULL PRIMARY KEY,
+                binary TEXT NOT NULL,
+                args JSON NOT NULL,
+                cwd TEXT NOT NULL,
+                inputs JSON NOT NULL
+            );",
+        ),
+    ]);
+
+    let db_path = "./ccelerate.db";
+    let mut conn = rusqlite::Connection::open(db_path)?;
+    db_migrations.to_latest(&mut conn)?;
+
+    let state = actix_web::web::Data::new(State {
+        conn: Arc::new(Mutex::new(conn)),
+    });
+
+    actix_web::HttpServer::new(move || {
         actix_web::App::new()
+            .app_data(state.clone())
             .service(route_index)
             .service(route_run)
     })
