842c7f519f0e67f568e2cda8cd17d6e3bd325472
Jacques Lucke <mail@jlucke.com>
2025-03-03 23:48:37 +0100

    initial tui

--- ccelerate_server/src/main.rs
+++ ccelerate_server/src/main.rs
@@ -4,7 +4,13 @@ use actix_web::HttpResponse;
 use anyhow::Result;
 use base64::prelude::*;
 use ccelerate_shared::RunRequestData;
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
 use parking_lot::Mutex;
+use ratatui::{
+    layout::Layout,
+    style::{Color, Style},
+    widgets::TableState,
+};
 use rusqlite_migration::{M, Migrations};
 
 mod parse_ar;
@@ -12,7 +18,15 @@ mod parse_gcc;
 mod path_utils;
 
 struct State {
+    address: String,
     conn: Arc<Mutex<rusqlite::Connection>>,
+    items: Arc<Mutex<Vec<TaskItem>>>,
+    items_table_state: Arc<Mutex<TableState>>,
+}
+
+struct TaskItem {
+    data: RunRequestData,
+    active: Arc<Mutex<bool>>,
 }
 
 #[actix_web::get("/")]
@@ -89,7 +103,7 @@ async fn route_run(
         }
     };
 
-    if let Some(output_path) = output_path {
+    if let Some(output_path) = &output_path {
         let conn = state.conn.lock();
         conn.execute(
             "INSERT OR REPLACE INTO Files (binary, path, cwd, args) VALUES (?1, ?2, ?3, ?4)",
@@ -102,8 +116,18 @@ async fn route_run(
         )
         .unwrap();
     }
-
-    println!("{:#?}", run_request);
+    let active = {
+        let mut items = state.items.lock();
+        items.push(TaskItem {
+            data: run_request.clone(),
+            active: Arc::new(Mutex::new(true)),
+        });
+        state.items_table_state.lock().select_last();
+        items.last().unwrap().active.clone()
+    };
+    scopeguard::defer! {
+        *active.lock() = false;
+    }
 
     let Ok(command) = tokio::process::Command::new(&run_request.binary)
         .args(&run_request.args)
@@ -138,6 +162,21 @@ async fn route_run(
     HttpResponse::Ok().json(&response_data)
 }
 
+async fn server_thread(state: actix_web::web::Data<State>) {
+    let state_clone = state.clone();
+    actix_web::HttpServer::new(move || {
+        actix_web::App::new()
+            .app_data(state.clone())
+            .service(route_index)
+            .service(route_run)
+    })
+    .bind(state_clone.address.clone())
+    .unwrap()
+    .run()
+    .await
+    .unwrap();
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let addr = format!("127.0.0.1:{}", ccelerate_shared::DEFAULT_PORT);
@@ -157,17 +196,75 @@ async fn main() -> Result<()> {
     db_migrations.to_latest(&mut conn)?;
 
     let state = actix_web::web::Data::new(State {
+        address: addr,
         conn: Arc::new(Mutex::new(conn)),
+        items: Arc::new(Mutex::new(Vec::new())),
+        items_table_state: Arc::new(Mutex::new(TableState::default())),
     });
 
-    actix_web::HttpServer::new(move || {
-        actix_web::App::new()
-            .app_data(state.clone())
-            .service(route_index)
-            .service(route_run)
-    })
-    .bind(addr)?
-    .run()
-    .await?;
+    tokio::spawn(server_thread(state.clone()));
+
+    let mut terminal = ratatui::init();
+
+    loop {
+        let state_clone = state.clone();
+        terminal
+            .draw(|frame| {
+                draw_terminal(frame, state_clone);
+            })
+            .expect("failed to draw terminal");
+        if crossterm::event::poll(std::time::Duration::from_millis(100)).unwrap() {
+            match crossterm::event::read().unwrap() {
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char('q'),
+                    ..
+                })
+                | Event::Key(KeyEvent {
+                    code: KeyCode::Esc, ..
+                })
+                | Event::Key(KeyEvent {
+                    code: KeyCode::Char('c'),
+                    modifiers: KeyModifiers::CONTROL,
+                    ..
+                }) => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+    ratatui::restore();
+
     Ok(())
+}
+
+fn draw_terminal(frame: &mut ratatui::Frame, state: actix_web::web::Data<State>) {
+    use ratatui::layout::Constraint::*;
+
+    let mut items = state.items.lock();
+    items.sort_by_key(|item| *item.active.lock());
+    let mut items_table_state = state.items_table_state.lock();
+
+    let vertical = Layout::vertical([Length(1), Min(0)]);
+    let [title_area, main_area] = vertical.areas(frame.area());
+    let text = ratatui::text::Text::raw(&state.address);
+    frame.render_widget(text, title_area);
+
+    let done_style = Style::new().fg(Color::Green);
+    let not_done_style = Style::new().fg(Color::Blue);
+
+    let table = ratatui::widgets::Table::new(
+        items.iter().map(|item| {
+            ratatui::widgets::Row::new([ratatui::text::Text::raw(item.data.args.join(" "))]).style(
+                if *item.active.lock() {
+                    not_done_style
+                } else {
+                    done_style
+                },
+            )
+        }),
+        [Percentage(100)],
+    );
+
+    frame.render_stateful_widget(table, main_area, &mut items_table_state);
 }
--- ccelerate_shared/src/lib.rs
+++ ccelerate_shared/src/lib.rs
@@ -1,13 +1,13 @@
 use std::path::PathBuf;
 
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 pub struct RunRequestData {
     pub binary: String,
     pub args: Vec<String>,
     pub cwd: PathBuf,
 }
 
-#[derive(Debug, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 pub struct RunResponseData {
     pub stdout: String,
     pub stderr: String,
