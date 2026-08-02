31ad3efe8e6cafc18e27a9e47f73ac2dc269b01f
Jacques Lucke <mail@jlucke.com>
2026-08-02 15:52:51 +0200

    progress

--- src/request_handler.cc
+++ src/request_handler.cc
@@ -38,15 +38,36 @@ static void pass_through_external_call(const ClientID &client_id,
   send_response_final(client_id, program_stdout, program_stderr, exit_code);
 }
 
-static void handle_eager_program_call(const Request &request) {
-  reproc::options options;
-  options.working_directory = request.working_dir.c_str();
-  vector<string> args;
-  args.push_back(string(to_string(request.program)));
-  for (const auto &arg : request.args) {
-    args.push_back(arg);
+static void pass_through_external_call(const ClientID &client_id,
+                                       const ProcessArgs &args,
+                                       const bool is_final) {
+  ProcessResult result = run_process(args);
+  string final_stdout = std::move(result.stdout);
+  string final_stderr = std::move(result.stderr);
+  if (const optional<error_code> ec = result.error()) {
+    final_stderr += "\n";
+    final_stderr += ec->message();
+    final_stderr += "\n";
   }
-  pass_through_external_call(request.client_id, args, std::move(options));
+  if (is_final) {
+    const int final_exit_code = result.exit_code().value_or(1);
+    send_response_final(client_id,
+                        std::move(final_stdout),
+                        std::move(final_stderr),
+                        final_exit_code);
+  } else {
+    send_response_incomplete(
+        client_id, std::move(final_stdout), std::move(final_stderr));
+  }
+}
+
+static void handle_eager_program_call(const Request &request) {
+  pass_through_external_call(request.client_id,
+                             ProcessArgs()
+                                 .arg(to_string(request.program))
+                                 .args(request.args)
+                                 .working_dir(request.working_dir),
+                             true);
 }
 
 static void handle_cmake_call(const Request &request) {
