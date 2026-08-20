74115acaff9393a3f994607ee73155615881a2c9
Jacques Lucke <mail@jlucke.com>
2026-08-20 21:53:01 +0200

    improve test

--- test_local_code/elaborated_forward_type.cc
+++ test_local_code/elaborated_forward_type.cc
@@ -1,13 +1,21 @@
 namespace A {
 
+template <typename T> struct Map {};
+
+using Alias = Map<struct B *>;
+
 struct Context {
   const struct B *gen;
+  Alias *alias;
 };
 
 struct B {
   int x;
 };
 
-void use(const B *p) { (void)p; }
+void use(const B *p, Alias *a) {
+  (void)p;
+  (void)a;
+}
 
 } // namespace A
